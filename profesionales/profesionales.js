8d482afbb076de3d7b9d3b9be14f52aec9b23590
valeria247 <84050084+valeria247@users.noreply.github.com>
2022-10-13 14:45:15 -0500

    select_change

--- profesionales/profesionales.js
+++ profesionales/profesionales.js
@@ -131,6 +131,79 @@ $('#departament').change(function (e) {
     })
 })
 
+/* Modal */
+
+const ciudad = {
+    ama: ["Seleccione Ciudad", "El Encanto", "La Chorrera", "La Pedrera", "La Victoria",
+        "Leticia", "Miriti", "Puerto Alegria", "Puerto Arica", "Puerto Nariño",
+        "Puerto Ssantander", "Taparaca"],
+    ant: ["Seleccione Ciudad", "Abriaquí", "Altamira", "Amalfi", "Angostura", "Anorí", "Anzá",
+        "Apartadó", "Aragón", "Arboletes", "Bajirá", "Bellavista", "Belmira",
+        "Berlín", "Betulia", "Briceño", "Builópolis", "Buriticá", "Caicedo", "Campamento",
+        "Carepa", "Carolina", "Caucasia", "Cañasgordas", "Cedeño", "Cestillal", "Chamuscados",
+        "Chigorodó", "Concordia", "Copacabana", "Currulao", "Cáceres", "Córdoba", "Córdova",
+        "Dabeiba", "Don Matías", "Ebéjico", "El Aro", "El Bagre", "El Brasil", "El Carmen", "El Cedro",
+        "El Oro", "El Salto", "El Tres", "Entrerrios", "Frontino", "Giraldo", "Guadalupe", "Guasabra",
+        "Gómez Plata", "Güintar", "Horizontes", "Ituango", "Jaiperá", "Jardín", "Juntas de Uramita",
+        "La Encarnación", "La Granja", "La Merced", "La Placita", "La Playa", "Labores", "Liborina",
+        "Llanadas", "Llanos de Cuivá", "Manguruma", "Marinilla", "Medellín", "Murri", "Mutatá", "Nariño",
+        "Necoclí", "Nutibara", "Ochalí", "Olaya", "Palmitas", "Pavarandocito", "Peque", "Pueblo Nuevo",
+        "Puerto Antioquia", "puerto Berrío", "Puerto Valdivia", "Quebrada Seca", "Quebradona", "Remedios",
+        "Rioverde", "Sabanalarga", "Saiza", "San Andrés", "San Jerónimo", "San José", "San Juan de Urabá",
+        "San Nicolás de Titumate", "San Pablo", "San Pedro", "San Pedro de Urabá", "Santa Ana",
+        "Santa Fé de Antioquia", "Santa Rita", "Santa Rosa de Osos", "Sevilla", "Sopetrán", "Sucre",
+        "Tabacal", "Tarazá", "Toledo", "Tonusco", "Arriba", "Turbo", "Urama", "Uramagrande", "Uramita",
+        "Urrao", "Valdivia", "Vigía del Fuerte", "Yalí", "Yarumal"],
+    ara: ["Seleccione Ciudad", "Arauca", "Arauquita", "Cravo Norte", "Fortul", "Puerto Rondon", "Saravena", "Tame"],
+    arc: ["Seleccione Ciudad", "San Andres", "Providencia"],
+    atl: ["Seleccione Ciudad", "Barranquilla", "Baranoa", "Campo de la Cruz", "Candelaria", "Galapa",
+        "Juan de Acosta", "Luruaco", "Malambo", "Malambo", "Palmar de Varela", "Piojó", "Polonuevo", "Ponedera",
+        "Puerto Colombia", "Repelón", "Sabanagrande", "Sabanalarga", "Santa Lucía", "Santo Tomás", "Soledad", "Suán",
+        "Tubará", "Usiacurí"],
+    bog: ["Seleccione Ciudad", "Bogota"],
+    bol: ["Seleccione Ciudad", "Achi", "Altos del rosario", "Arenal", "Arjona", "Arroyohondo", "Barranco de loba", "Calamar",
+        "Cantagallo", "Cicuco", "Córdoba", "Clemencia", "El carmen de bolívar", "El guamo", "El peñón", "Hatillo de loba",
+        "Magangué", "Mahates", "Margarita", "María la baja", "Montecristo", "Mompos", "Morales", "Norosi", "Pinillos",
+        "Regidor", "Río viejo", "San cristóbal", "San estanislao", "San fernando", "San jacinto", "San jacinto del cauca",
+        "San juan nepomuceno", "San martín de loba", "San pablo", "Santa catalina", "Santa rosa", "Santa rosa del sur",
+        "Simití", "Soplaviento", "Talaigua nuevo", "Tiquisio", "Turbaco", "Turbaná", "Villanueva", "Zambrano"],
+    boy: ["A", "E", "V", "E"],
+    cal: ["M", "A", "E", "C"],
+    caq: ["a", "a", "a", "a"],
+    cas: ["d", "e", "f", "g"],
+    cau: ["h", "i", "j", "k"],
+    ces: ["h", "i", "j", "k"],
+    cho: ["h", "i", "j", "k"],
+    cor: ["A", "E", "V", "E"],
+    cun: ["M", "A", "E", "C"],
+    guai: ["a", "a", "a", "a"],
+    guav: ["d", "e", "f", "g"],
+    hui: ["h", "i", "j", "k"],
+    gua: ["h", "i", "j", "k"],
+    met: ["h", "i", "j", "k"],
+    nar: ["A", "E", "V", "E"],
+    nor: ["M", "A", "E", "C"],
+    put: ["a", "a", "a", "a"],
+    qui: ["d", "e", "f", "g"],
+    ris: ["h", "i", "j", "k"],
+    sana: ["h", "i", "j", "k"],
+    sant: ["A", "E", "V", "E"],
+    suc: ["M", "A", "E", "C"],
+    tol: ["a", "a", "a", "a"],
+    val: ["d", "e", "f", "g"],
+    vau: ["h", "i", "j", "k"],
+    vic: ["h", "i", "j", "k"]
+}
+$('#depart').change(function (e) {
+    let modelList = document.getElementById("ciudad");
+    while (modelList.options.length) {
+        modelList.remove(0);
+    }
+    ciudad[$(this).val()].map(city => {
+        $('#ciudad').append(`<option value="${city}">${city}</option>`);
+    })
+})
+
 const area = {
     bac: ["a", "b", "c", "d", "e"],
     en_aux: ["f", "g", "h", "i"],
