22954184a63db189a1d312271bbd3fb91259b53f
stirwin <vaciosub@gmail.com>
2022-10-31 11:27:09 -0500

    agendamiento

--- application/views/template/agendamiento.php
+++ application/views/template/agendamiento.php
@@ -1,6 +1,6 @@
  <main>
        <div class="contenido">
-        <h1>estoy en agendamiento</h1><br>
+        <h1>estoy en agendamiento ya funciona</h1><br>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam sapiente cumque dicta animi explicabo sequi. Ex amet et, dolor eligendi commodi consectetur quo voluptatibus, cum nemo porro veniam at blanditiis?</p> <br>
         <p>
 
